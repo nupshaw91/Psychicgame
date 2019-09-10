853472b31d0c06cf7e172ed13a33d822010d9106
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-10 16:51:37 -0400

    skeleton and features

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Create object of the game to be called upon throughout game
+var game = {
+    Titles: ["It", "Us", "House Party", "Candyman", "Black Panther", "Lion King", "Finding Nemo"],
+    Guesses: 6,
+
+
+}
+
+//Create the randomizer to pick the titles for the guessing
+Math.floor
+
+//Reset the guesses once the correct movie title has been guessed
+
