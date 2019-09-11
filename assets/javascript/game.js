01381a5665fb1e045762fe9421f54065c3c2f0cf
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-10 20:12:29 -0400

    changing games due to time restraints

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,21 @@
-// Create object of the game to be called upon throughout game
-var game = {
-    Titles: ["It", "Us", "House Party", "Candyman", "Black Panther", "Lion King", "Finding Nemo"],
-    Guesses: 6,
+// Create varibles of the game to be called upon throughout game
+    var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p",
+    "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var wins = 0;
+    var losses = 0;
+    var ties = 0;
+
+    var wins = document.getElementsByClassName("wins");
+    var losses = document.getElementsByClassName("losses");
+    var ties = document.getElementsByClassName("ties");
+    var computerMind = document.getElementsByClassName("computermind");
+    var userMind = document.getElementsByClassName("usermind");
+
+    var directionsText = document.getElementsByClassName("directions-text");
+    
+   
+    var wins = document.getElementsByClassName("wins");
+    var losses = document.getElementsByClassName("losses");
+    var ties = document.getElementsByClassName("ties");
 
 
-}
-
-//Create the randomizer to pick the titles for the guessing
-Math.floor
-
-//Reset the guesses once the correct movie title has been guessed
-
-//change picture to go along with guess count
